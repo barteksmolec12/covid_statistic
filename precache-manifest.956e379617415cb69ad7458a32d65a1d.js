4b56a81b96666258bb598eb6048064615271e504
barteksmolec12 <barteksmolec@o2.pl>
2020-05-08 22:23:10 +0200

    Updates

---
+++ precache-manifest.956e379617415cb69ad7458a32d65a1d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aba59f23addebb756723d2ddefbe2719",
+    "url": "/covid_statistic/index.html"
+  },
+  {
+    "revision": "674e90ef6da30da40369",
+    "url": "/covid_statistic/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "1061b81552577155c038",
+    "url": "/covid_statistic/static/css/main.6e3a21c0.chunk.css"
+  },
+  {
+    "revision": "674e90ef6da30da40369",
+    "url": "/covid_statistic/static/js/2.8b332b0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid_statistic/static/js/2.8b332b0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1061b81552577155c038",
+    "url": "/covid_statistic/static/js/main.22418074.chunk.js"
+  },
+  {
+    "revision": "d95498e97ac27706b8bb",
+    "url": "/covid_statistic/static/js/runtime-main.2a5d24bd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid_statistic/static/media/logo.5d5d9eef.svg"
+  }
+]);
